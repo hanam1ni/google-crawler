078e0c8f7aa397c51a39230c4b6bbf99f73ca115
Micky Jittjana <immicky.a@gmail.com>
2020-11-16 16:25:23 +0700

    [Backend] As a user, I can filter search result URL by keyword (#11)
    * Filter keyword with url
    
    * Add controller and context tests
    
    * Group keyword filter in params

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -2,5 +2,6 @@ ExUnit.start()
 Faker.start()
 Ecto.Adapters.SQL.Sandbox.mode(GoogleCrawler.Repo, :manual)
 
-Mimic.copy(GoogleCrawler.SearchResults.ParserSupervisor)
+Mimic.copy(GoogleCrawler.Keywords)
 Mimic.copy(GoogleCrawler.Keywords.ScraperSupervisor)
+Mimic.copy(GoogleCrawler.SearchResults.ParserSupervisor)
